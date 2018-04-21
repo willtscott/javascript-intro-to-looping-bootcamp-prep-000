3ea0b5ee86683ea52aed2416061ba85a1150c324
wtscott@gmail.com <wtscott@gmail.com>
2018-04-21 23:10:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,3 +14,13 @@ function whileLoop(num){
   return 'done' }
 
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop() }
+  while (array.length > 0 && maybeTrue());
+  return array
+}
